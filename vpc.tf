5094b89bc6549dad24e38fe5daebcb253fa067bc
BBBunny Dev <BBBunny@dev.local>
2022-12-09 21:37:29 +0700

    change try cidr_block of my aws VPCs

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "devops-training" {
-    # setup cidr_block network
-  cidr_block = "10.0.0.0/16"
+  # setup cidr_block network
+  # cidr_block = "10.0.0.0/16"
+  cidr_block = "172.31.0.0/16"
 
   tags = {
     Name = "training"
